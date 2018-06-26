c873504dd6abc33aea9f24817f32b7688c5cb08a
krongpawornrat <phonemiw2278@gmail.com>
2018-06-26 11:42:48 +0700

    Hello World

---
+++ src/lab1/myCode.java
@@ -0,0 +1,22 @@
+package lab1;
+
+public class myCode {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World.");
+
+        System.out.println("Pawornrat Homhual");
+        System.out.println("361211760038");
+        System.out.println("MIT211");
+        System.out.println("RMUTSV");
+
+
+    }//main
+
+
+
+
+
+
+
+}//class
