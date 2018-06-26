12ab4767a8374b2d7dd64c4c2f9bad715df96878
krongpawornrat <phonemiw2278@gmail.com>
2018-06-26 11:53:20 +0700

    Delete message RMUTSV

--- src/lab1/myCode.java
+++ src/lab1/myCode.java
@@ -4,11 +4,11 @@ public class myCode {
     public static void main(String[] args) {
 
         System.out.println("Hello World.");
-
         System.out.println("Pawornrat Homhual");
         System.out.println("361211760038");
         System.out.println("MIT211");
-        System.out.println("RMUTSV");
+
+
 
 
     }//main
